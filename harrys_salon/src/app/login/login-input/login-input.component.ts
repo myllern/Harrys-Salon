cebb76751b683e4612dcf8ab6a49e8422c79459f
myllern <49235951+myllern@users.noreply.github.com>
2021-03-04 18:12:24 +0100

    fix

--- harrys_salon/src/app/login/login-input/login-input.component.ts
+++ harrys_salon/src/app/login/login-input/login-input.component.ts
@@ -15,14 +15,13 @@ import {
 export class LoginInputComponent implements OnInit {
   form = new FormGroup({
     username: new FormControl(''),
-    password: new FormControl('', [Validators.required, Validators.min(3)]),
+    password: new FormControl(''),
     //password2: new FormControl('', [Validators.required, Validators.min(3)]),
   });
 
   constructor() {}
 
   ngOnInit(): void {}
-
   hide = true;
   get passwordInput() { return this.form.controls.password2; } 
   onSubmit() {
