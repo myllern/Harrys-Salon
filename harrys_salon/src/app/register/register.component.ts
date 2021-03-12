756d42be4cfc9835cf09a48b85e674d9612ba259
Hannes <dat15hlu@student.lu.se>
2021-03-12 12:25:30 +0100

    cant register while logged in and changed to input from textarea

--- harrys_salon/src/app/register/register.component.ts
+++ harrys_salon/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
